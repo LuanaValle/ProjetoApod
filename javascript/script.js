b585ceec421d2a8f1a02b6d85806539ea0199437
LuanaValle <luanavalle.mendes@gmail.com>
2021-12-14 16:52:03 -0300

    JS pronto

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,15 @@
 $(
     "#selecionar"
 ).click (function (){
-    
+    let data = $("#data").val()
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=0W6RBXPTSBew7K94jthFEke7CNh2TcANIzAv7gKf&date=${data}`,
+        type: "GET", 
+        success: function(resultado){
+            $(".paragrafo").text(resultado.explanation),
+            $("#imagem").attr("src", resultado.url),
+            $(".titulofoto").text(resultado.title)
+        }
+    })
 }
     )
